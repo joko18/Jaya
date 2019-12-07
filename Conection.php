61d7f1d34fa9b4bb6b8bcf3c750f1ee0160391c1
Jaya67 <58602080+Jaya67@users.noreply.github.com>
2019-12-07 11:20:01 +0700

    Update Conection.php

--- Conection.php
+++ Conection.php
@@ -1,8 +1,8 @@
 <?php
 $sever = "localhost";
 $user = "id11853233_jaya";
-$pass = "Dijay1867";
-$database = "id11853233_dbtugasssip";
+$pass = "Drogz1867";
+$database = "id11853233_ssipgrouph";
 
-$conn = mysqli_connect($sever, $user, $pass, $database,);  
+$conn = mysqli_connect($sever, $user, $pass, $database);
 ?>
