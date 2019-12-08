1c0cbf6bc042629f1553ec212970b166ffeaa2ce
Jaya67 <58602080+Jaya67@users.noreply.github.com>
2019-12-08 18:27:19 +0700

    Update Login.php

---
+++ Login.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<form action="LoginProses.php" method="POST">
+<label>
+User    :<input type="text" name="user">
+Password:<input type="text" name="pass">
+<label>
+<input type="submit" name="Login" values="Login">
+<body>
+</html>
