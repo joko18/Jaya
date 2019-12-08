32c71be8b123bac638d35ce7585e96d94290bc5c
Jaya67 <58602080+Jaya67@users.noreply.github.com>
2019-12-08 18:27:46 +0700

    Create LoginProses.php

---
+++ LoginProses.php
@@ -0,0 +1,25 @@
+<?php
+include("Conection.php");
+
+if(isset($_POST['Login'])){
+    $User=$_POST['user'];
+    $Pass=$_POST['pass'];
+
+    $sql= "SELECT * FROM tbl_user where ID_User='$User' and Password='$Pass'";
+    $query = mysqli_query($conn,$sql);
+    $arrayData = (msqli_fetch_array($query));
+    session_start()
+    $_SESSION['ID'] = $arrayData[' ID_User '];
+
+    if ($arrayData[' Jabata '] = "Dosen"){
+            header("location:Dosen.php");
+    }else{
+
+            header("location=Admin.php");
+
+    }
+
+    
+}
+
+?>
