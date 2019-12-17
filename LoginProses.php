b5882141216c10007f08be339d88ad9a17d0b08a
Jaya67 <58602080+Jaya67@users.noreply.github.com>
2019-12-17 10:34:52 +0700

    Update LoginProses.php

--- LoginProses.php
+++ LoginProses.php
@@ -1,24 +1,24 @@
 <?php
-include "Conection.php";
+include 'Conection.php';
 
 if(isset($_POST['Login'])){
     $User=$_POST['user'];
     $Pass=$_POST['pass'];
 
-    $sql= "SELECT * FROM tbl_User where ID_User='$User' and Password='$Pass'";
+    $sql= "SELECT * FROM tbl_user where Nama_User='$User' and password='$Pass'";
     $query = mysqli_query($conn,$sql);
-    $arrayData = (mysqli_fetch_array($query));
+    $valid = mysqli_num_rows($query);
+    if($valid > 0 ){
     session_start();
+    $arrayData = (mysqli_fetch_array($query));
     $_SESSION['ID'] = $arrayData['ID_User'];
+    
+    header("location:Admin.php");
 
-    if ($arrayData['Jabatan'] == "Dosen"){
-            header("location:Dosen.php");
-    }else{
-
-            header("location=Admin.php");
-
-
-        }
+    
         
     }
+}
+
+?>
     
