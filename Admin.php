0a5a34b58a9827b5015a90c56d8bd69349340b77
Jaya67 <58602080+Jaya67@users.noreply.github.com>
2019-12-17 10:33:44 +0700

    Update Admin.php

--- Admin.php
+++ Admin.php
@@ -1,27 +1,32 @@
 <?php
 include "Conection.php";
-$result = mysqli_query($conn, "SELECT * FROM tbl_admin");
+$result = mysqli_query($conn, "SELECT Nama_Mahasiswa,NIM,Nama_Prodi,Jurusan,Email FROM tbl_admin
+INNER JOIN tbl_prodi ON ID_Matakuliah=ID_Prodi");
 ?>
 <html>
 <head>    
-    <title>Admin</title>
+    <title>Mahasiswa</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+    <script type="text/javascript" src="js/jquery.js"></script>
+	<script type="text/javascript" src="js/bootstrap.js"></script>
 </head>
 
 <body>
 
-    <table width='80%' border=1>
+    <table class="table">
 
     <tr>
-       <th>Nama Mahasiswa</th> <th>id</th> <th>Jurusan</th> <th>Email</th> <th>Info</th>
+       <th>Nama Mahasiswa</th> <th>NIM</th> <th>Nama Prodi</th> <th>Jurusan</th><th>Email</h><th>Info</th>
     </tr>
     <?php  
     while($user_data = mysqli_fetch_array($result)) {         
         echo "<tr>";
-		echo "<td>".$user_data['Nama']."</td>";
-		echo "<td>".$user_data['id']."</td>";
+		echo "<td>".$user_data['Nama_Mahasiswa']."</td>";
+		echo "<td>".$user_data['NIM']."</td>";
+        echo "<td>".$user_data['Nama_Prodi']."</td>";
         echo "<td>".$user_data['Jurusan']."</td>";
         echo "<td>".$user_data['Email']."</td>"; 
-        echo "<td><a href='edit.php?id=$user_data[id]'>Update</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";        
+        echo "<td><a href='edit.php?id=$user_data[NIM]'>Update</a> | <a href='delete.php?id=$user_data[NIM]'>Delete</a></td></tr>";        
     } 
     ?>
 	
